8c0f839ae7b43a22061fc99f9c4cedac693da6fb
umi-chaahan <umityaahan@gmail.com>
2020-11-06 13:45:11 +0900

    for bug fix

--- docs/assets/fire.glsl.js
+++ docs/assets/fire.glsl.js
@@ -1,4 +1,46 @@
 ---
+name: Plasma3
+type: fragment
+author: triggerHLM (https://www.shadertoy.com/view/MdXGDH)
+---
+
+precision mediump float;
+
+uniform float time;
+uniform vec2 resolution;
+
+varying vec2 fragCoord;
+
+const float PI = 3.14159265;
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+
+    float time = time * 0.2;
+
+    float color1, color2, color;
+    
+    color1 = (sin(dot(fragCoord.xy,vec2(sin(time*3.0),cos(time*3.0)))*0.02+time*3.0)+1.0)/2.0;
+    
+    vec2 center = vec2(640.0/2.0, 360.0/2.0) + vec2(640.0/2.0*sin(-time*3.0),360.0/2.0*cos(-time*3.0));
+    
+    color2 = (cos(length(fragCoord.xy - center)*0.03)+1.0)/2.0;
+    
+    color = (color1+ color2)/2.0;
+
+    float red   = (cos(PI*color/0.5+time*3.0)+1.0)/2.0;
+    float green = (sin(PI*color/0.5+time*3.0)+1.0)/2.0;
+    float blue  = (sin(+time*3.0)+1.0)/2.0;
+    
+    fragColor = vec4(red, green, blue, 1.0);
+}
+
+void main(void)
+{
+    mainImage(gl_FragColor, fragCoord.xy);
+    gl_FragColor.a = 1.0;
+}
+
+---
 name: Flower Plasma2
 type: fragment
 author: epsilum (https://www.shadertoy.com/view/Xdf3zH)
