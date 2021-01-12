14e9b0dc2f4acd225ee431b04df65defac5c9350
umi tyaahan <umityaahan@gmail.com>
2021-01-12 21:36:37 +0900

    fix webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,18 +2,22 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // ビルド対象を変える場合はここを変更する
-// const buildtarget = './src/boilerplate/';
-// const buildtarget = './src/sandbox/';
-const buildtarget = './src/games/flash/';
-// const buildtarget = './src/games/tutorial/';
+const target = 'flash';
+// const target = '../boilerplate';
+// const target = 'sandbox';
+// const target = 'flash';
+// const target = 'tutorial';
+
+const buildPath = path.resolve(__dirname, `../src/games/${target}/`);
+const outputPath = path.resolve(__dirname, '../public/');
 
 module.exports = {
   // エントリポイント 依存関係整理の起点にするファイル
-  entry: buildtarget + 'game.ts',
+  entry: buildPath + '/game.ts',
   output: {
     // モジュールバンドルを行った結果を出力する場所やファイル名の指定
     // "__dirname"はこのファイルが存在するディレクトリを表すnode.jsで定義済みの定数
-    path: path.resolve(__dirname, '../public'),
+    path: outputPath,
     filename: 'js/bundle.js',
   },
   // モジュールに適用するルールの設定（ここではローダーの設定を行う事が多い）
@@ -25,7 +29,7 @@ module.exports = {
   },
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [{ from: 'assets/', to: 'assets/', context: buildtarget }],
+      patterns: [{ from: 'assets/', to: 'assets/', context: buildPath }],
     }),
   ],
   // モジュールとして扱いたいファイルの拡張子を指定する
@@ -37,7 +41,7 @@ module.exports = {
 
   devServer: {
     // webpack-dev-serverの公開フォルダ
-    contentBase: path.resolve(__dirname, '../public'),
+    contentBase: outputPath,
     host: '0.0.0.0', // for virtualbox
     port: 5000, // for virtualbox
     // open: true,
